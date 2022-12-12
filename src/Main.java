e6e76b1521822d1048a9fb9109740f105bfea5df
sterben <mr.azhikulov@gmail.com>
2022-12-12 19:01:42 +0600

    Create new footer

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static int[][] result = new int[2][];
-    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-
+    public static void printArraySum(int[] data) {
+        int sum = 0;
+        for (int i = 0; i < data.length; i++) {
+            sum = sum + data[i];
+        }
+        System.out.println(sum);
+    }
 
     public static void main(String[] args) {
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
+        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30};
+        printArraySum(months);
+
     }
 }
-
